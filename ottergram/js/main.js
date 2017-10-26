242aa42925f6f4026e5081cf6cb0859951196ff8
wupuyi <wupuyi@foxmail.com>
2017-10-26 23:48:08 +0800

    commit

---
+++ ottergram/js/main.js
@@ -0,0 +1,10 @@
+var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+
+var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+
+var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+
+function setDetails() {
+    // 使用严格模式
+    'use strict'; 
+}
