cf124d2bf7038067598e3816508fad55c973d99f
wupuyi <wupuyi@foxmail.com>
2017-10-27 14:40:23 +0800

    commit

--- ottergram/js/main.js
+++ ottergram/js/main.js
@@ -4,7 +4,54 @@ var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
 
-function setDetails() {
+function setDetails(imageUrl, titleText) {
     // 使用严格模式
     'use strict'; 
+    var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+    detailImage.setAttribute('src', imageUrl);
+
+    var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+    detailTitle.textContent = titleText;
 }
+
+function imageFromThumb(thumbnail) {
+    'use strict';
+    return thumbnail.getAttribute('data-image-url');
+}
+
+function titleFromThumb(thumbnail) {
+    'use strict';
+    return thumbnail.getAttribute('data-image-title');
+}
+
+
+function setDetailsFromThumb(thumbnail) {
+    'use strict';
+    setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
+}
+
+function addThumbClickHandler(thumb) {
+    'use strict';
+    thumb.addEventListener('click', function(event) {
+        event.preventDefault();
+        setDetailsFromThumb(thumb);
+    });
+}
+
+function getThumbnailsArray() {
+    'use strict';
+    var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+    //把元素节点列表转换为数组
+    var thumbnailArray = [].slice.call(thumbnails);
+    return thumbnailArray;
+}
+
+function initializeEvents() {
+    'use strict';
+    var thumbnails = getThumbnailsArray();
+    thumbnails.forEach(addThumbClickHandler);
+
+}
+
+
+initializeEvents();
