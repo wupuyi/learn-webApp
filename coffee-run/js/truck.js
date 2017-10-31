669db15bc5e34c3ab0da0e397e21ed78e65bd644
wupuyi <wupuyi@foxmail.com>
2017-10-31 21:45:14 +0800

    commit

--- coffee-run/js/truck.js
+++ coffee-run/js/truck.js
@@ -8,14 +8,23 @@
     }
 
     Truck.prototype.createOrder = function(order) {
-        console.log('Adding order for' + order.emailAddress);
+        console.log('Adding order for ' + order.emailAddress);
         this.db.add(order.emailAddress, order);
-    }
+    };
 
     Truck.prototype.deliverOrder = function(customerId) {
-        console.log('Delivering order for' + customerId);
+        console.log('Delivering order for ' + customerId);
         this.db.remove(customerId);
-    }
+    };
+
+    Truck.prototype.printOrders = function(){
+        var customerIdArray = Object.keys(this.db.getAll());
+
+        console.log('Truck #' + this.truckId + ' has pending orders:');
+        customerIdArray.forEach(function(id) {
+            console.log(this.db.get(id));
+        });
+    };
 
     App.Truck = Truck;
     window.App = App;
