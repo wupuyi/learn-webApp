ee9d5ce9edc7282e78bd738d18e1578eba8a8a6e
wupuyi <wupuyi@foxmail.com>
2017-11-01 17:09:32 +0800

    commit

--- coffee-run/js/main.js
+++ coffee-run/js/main.js
@@ -1,10 +1,17 @@
 (function(window) {
     'use strict';
+    var FORM_SELECTOR = '[data-coffee-order="form"]';
     var App = window.App;
     var Truck = App.Truck;
     var DataStore = App.DataStore;
+    var FormHandler = App.FormHandler;
     var myTruck = new Truck('ncc-1701', new DataStore());
-    var myTruck2 = new Truck('TANK', new DataStore());
+    // 下面一行为  测试
+    // var myTruck2 = new Truck('TANK', new DataStore());
     // 把myTruck暴露到全局命名空间
     window.myTruck = myTruck;
+    var formHandler = new FormHandler(FORM_SELECTOR);
+
+    formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
+    console.log(formHandler);
 })(window);
